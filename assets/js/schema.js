246d571d4f927a5e13f7c1e7c6153cb159f65d94
ARIF CAHYO PRASETYO <arif.0009@students.amikom.ac.id>
2020-02-26 08:57:36 +0700

    up sitemap

---
+++ assets/js/schema.js
@@ -0,0 +1,13 @@
+{
+	"@context": "http://schema.org",
+    "@type": "Organization",
+    "name": "cahyosoft",
+    "url": "http://cahyosoft.my.id",
+    "address": "Yogyakarta",
+    "sameAs": [
+      "https://www.facebook.com/mas.cahyo.5473",
+      "https://twitter.com/mascahyo15",
+      "https://www.linkedin.com/in/arif-cahyo-prasetyo-b7413a177/",
+      "https://www.instagram.com/cahyomas/"	
+    ]
+  }
